da61793f1d241d909e3a90132485e3f9a6d539b4
George Ridgway <georger361@gmail.com>
2016-05-18 13:28:16 -0400

    Started Working on StudyGuide Object + Class

---
+++ java/studyGuide/StudyGuide.java
@@ -0,0 +1,79 @@
+package studyGuide;
+
+import project.schedule.classes.SchoolClass;
+import project.user.User;
+
+public class StudyGuide 
+{
+	private User author;
+	private SchoolClass subject;
+	private String content;
+	private int rating;
+	
+	public StudyGuide(User author, SchoolClass subject, String content, int rating) 
+	{
+		super();
+		this.author = author;
+		this.subject = subject;
+		this.content = content;
+		this.rating = rating;
+	}
+	
+	public User getAuthor()
+	{
+		return author;
+	}
+
+	public void setAuthor(User author)
+	{
+		this.author = author;
+	}
+
+	public SchoolClass getSubject()
+	{
+		return subject;
+	}
+
+	public void setSubject(SchoolClass subject) 
+	{
+		this.subject = subject;
+	}
+
+	public String getContent() 
+	{
+		return content;
+	}
+
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+
+	public int getRating()
+	{
+		return rating;
+	}
+
+	public void setRating(int rating)
+	{
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "StudyGuide [author=" + author + ", subject=" + subject + ", content=" + content + ", rating=" + rating
+				+ "]";
+	}
+	
+	public boolean equals(StudyGuide c)
+	{
+		boolean equals = false;
+		if (this.content.equals(c.content))
+		{
+			equals = true;
+		}
+		return equals;
+	}
+	
+}
