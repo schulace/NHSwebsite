252a31ddf44dad8768b6169d77ca85a05101ffc2
schulace <schulace2@gmail.com>
2016-05-19 14:40:58 -0400

    added no-parameter constructor to teacher + stopped extending user

--- java/project/user/Teacher.java
+++ java/project/user/Teacher.java
@@ -6,28 +6,27 @@ import java.util.ArrayList;
 import project.schedule.classes.SchoolClass;
 
 
-public class Teacher extends User 
+public class Teacher
 { //we may not even need this, but its nice to have (just in case)
-	private ArrayList<SchoolClass> teachClasses;
+	private String name;
+	private String school;
+	private Gender gender;
 	
-	public Teacher(String n, Date d, String school, Gender g, ArrayList<SchoolClass> t)
+	public Teacher(String n, String school, Gender g)
 	{
-		super(n, d, school, g);
-		this.teachClasses = t;
-	}
-
-	public ArrayList<SchoolClass> getTeachClasses() 
-	{
-		return teachClasses;
-	}
-
-	public void setTeachClasses(ArrayList<SchoolClass> teachClasses) 
-	{
-		this.teachClasses = teachClasses;
+		this.name = n;
+		this.school = school;
+		this.gender = g;
 	}
 	
-	public void rateTutor(Tutor placeholder, int score, String review)
+	public Teacher()
 	{
-		placeholder.teacherReview(score, review);
+		this("test_teacher", "GHS", Gender.MALE);
 	}
+
+//TODO george put this in the admininstrator thing.
+//	public void rateTutor(Tutor placeholder, int score, String review)
+//	{
+//		placeholder.teacherReview(score, review);
+//	}
 }
