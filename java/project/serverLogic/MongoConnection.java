a73b957c5d4215f078920f72046682c089e92100
Josh King <joshuaiking56@gmail.com>
2016-05-20 13:43:41 -0400

    Added more connection stuff
    Write whole json string is broken tho

--- java/project/serverLogic/MongoConnection.java
+++ java/project/serverLogic/MongoConnection.java
@@ -1,11 +1,31 @@
 package project.serverLogic;
 
+import com.mongodb.DB;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import com.mongodb.*;
+import com.google.gson.*;
+import org.bson.*;
 
 public class MongoConnection {
-
-	MongoClient mongoClient = new MongoClient( "localhost" );
-	MongoDatabase database = mongoClient.getDatabase("mydb");
+	
+	public boolean sendtodb(String jsonstring, int userid){
+		
+		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
+		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
+		MongoCollection<Document> collection = database.getCollection("test");//
+		
+		
+		
+//		Document dbObject = (DBObject)JSON.parse(jsonstring);
+//		collection.insertOne(dbObject);
+		
+		return false;
+		
+	}
+	
 	
 }
