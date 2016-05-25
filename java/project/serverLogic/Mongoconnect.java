47bd1f6522597a45d9cb5929f74e31dda15459cf
Josh King <joshuaiking56@gmail.com>
2016-05-25 11:06:24 -0400

    Made everything nonstatic, unfcked stuff, documentation

--- java/project/serverLogic/Mongoconnect.java
+++ java/project/serverLogic/Mongoconnect.java
@@ -11,7 +11,12 @@ import static com.mongodb.client.model.Filters.*;
 //	You can pass 0, 1, 2 or 3 variables to the constructor. If nothing is passed, it uses all default values,
 //	But you can also specify the collection, database, and host in that order.
 //
-//	Methods: getDbInfo(), insertToDb(), insertToDb(String json), getUserCount(), getFromDb(String field, String value)
+//	Methods: 
+//	getDbInfo(), returns String dbinfo;
+//	insertToDb(), returns nothing;
+//	insertToDb(String json), returns nothing;
+//	getUserCount(), returns int count;
+//	getFromDb(String field, String value), returns String json;
 
 
 public class Mongoconnect {
@@ -44,39 +49,39 @@ public class Mongoconnect {
 		this.usedcoll = calledcoll;//testdb
 	}
 	
-	public static MongoCollection<Document> getConnection(){//sets up initial connection
-		MongoClient mongoClient = new MongoClient(host); //connects to client on localhost
-		MongoDatabase database = mongoClient.getDatabase(dbname);//gets db called mydb
-		MongoCollection<Document> collection = database.getCollection(usedcoll);//replace with not tes
+	public MongoCollection<Document> getConnection(){//sets up initial connection
+		MongoClient mongoClient = new MongoClient(this.host); //connects to client on localhost
+		MongoDatabase database = mongoClient.getDatabase(this.dbname);//gets db called mydb
+		MongoCollection<Document> collection = database.getCollection(this.usedcoll);//replace with not tes
 		return collection;
 	}
 	
-	public static String getDbInfo(){//get db info, probably never gonna be used, might commend out
-		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
-		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
+	public String getDbInfo(){//get db info, probably never gonna be used, might commend out
+		MongoClient mongoClient = new MongoClient(this.host); //connects to client on localhost
+		MongoDatabase database = mongoClient.getDatabase(this.dbname);//gets db called mydb
 		String info = database.getName();//just make a string with the name of the db
 		return info;//return
 	}
 	
-	public static void insertToDb(){//send data to database, test method using ServerStart.getTestJson()
+	public void insertToDb(){//send data to database, test method using ServerStart.getTestJson()
 		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(ServerStart.getTestJson()); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
 	
-	public static void insertToDb(String json){//send data to database
+	public void insertToDb(String json){//send data to database
 		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(json); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
 	
-	public static long getUserCount(){//gets the number of users in a db
+	public long getUserCount(){//gets the number of users in a db
 		MongoCollection<Document> collection = getConnection();//retrieve a collection
 		long usercount = collection.count();
 		return usercount;
 	}
 	
-	public static String getfromdb(String field, String value){//for example, getfromdb(username, john);
+	public String getfromdb(String field, String value){//for example, getfromdb(username, john);
 		MongoCollection<Document> collection = getConnection();//retrieve a collection
 		Document myDoc = collection.find(eq(field,value)).first();
 		return myDoc.toJson();
