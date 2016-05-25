3913ce3c0a79b360e1de8f965b7f3af3767d080f
Josh King <joshuaiking56@gmail.com>
2016-05-25 10:24:36 -0400

    Organization

--- java/project/serverLogic/Mongoconnect.java
+++ java/project/serverLogic/Mongoconnect.java
@@ -7,6 +7,12 @@ import org.bson.*;
 
 public class Mongoconnect {
 	
+	public static MongoCollection<Document> getConnection(){//sets up initial connection
+		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
+		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
+		MongoCollection<Document> collection = database.getCollection("test1");//replace with not tes
+		return collection;
+	}
 	
 	public static String getdbinfo(){//get db info, probably never gonna be used, might commend out
 		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
@@ -15,27 +21,20 @@ public class Mongoconnect {
 		return info;//return
 	}
 	
-	public static void sendtodb(){//send data to database
-		MongoCollection<Document> collection = getconnection();
+	public static void inserttodb(){//send data to database, test method using ServerStart.getTestJson()
+		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(ServerStart.getTestJson()); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
-
-	public static MongoCollection<Document> getconnection(){
-		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
-		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
-		MongoCollection<Document> collection = database.getCollection("test1");//replace with not tes
-		return collection;
-	}
 	
-	public static void sendtodb(String json){//send data to database
-		MongoCollection<Document> collection = getconnection();
+	public static void inserttodb(String json){//send data to database
+		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(json); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
 	
 	public static long getusercount(){//gets the number of users in a db
-		MongoCollection<Document> collection = getconnection();//retrieve a collection
+		MongoCollection<Document> collection = getConnection();//retrieve a collection
 		long usercount = collection.count();
 		return usercount;
 	}
