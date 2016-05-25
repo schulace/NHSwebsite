f795c92567acdf8e3ff52405174ad3f2fe8dfe3b
Josh King <joshuaiking56@gmail.com>
2016-05-25 10:53:23 -0400

    Documentation,  added constructors,  organization

--- java/project/serverLogic/Mongoconnect.java
+++ java/project/serverLogic/Mongoconnect.java
@@ -4,44 +4,82 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.*;
+import static com.mongodb.client.model.Filters.*;
+
+//	Here's how do:
+//	
+//	You can pass 0, 1, 2 or 3 variables to the constructor. If nothing is passed, it uses all default values,
+//	But you can also specify the collection, database, and host in that order.
+//
+//	Methods: getDbInfo(), insertToDb(), insertToDb(String json), getUserCount(), getFromDb(String field, String value)
+
 
 public class Mongoconnect {
 	
+	private static String host;
+	private static String dbname;
+	private static String usedcoll;
+	
+	public Mongoconnect(){
+		this.host = "localhost";//default host
+		this.dbname = "mydb";//defualtdb
+		this.usedcoll = "test1";//testdb
+	}
+	
+	public Mongoconnect(String calledcoll){
+		this.host = "localhost";//default host
+		this.dbname = "mydb";//defualtdb
+		this.usedcoll = calledcoll;//testdb
+	}
+	
+	public Mongoconnect(String calledcoll, String db){
+		this.host = "localhost";//default host
+		this.dbname = db;//defualtdb
+		this.usedcoll = calledcoll;//testdb
+	}
+	
+	public Mongoconnect(String calledcoll, String db, String location){
+		this.host = location;//default host
+		this.dbname = db;//defualtdb
+		this.usedcoll = calledcoll;//testdb
+	}
+	
 	public static MongoCollection<Document> getConnection(){//sets up initial connection
-		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
-		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
-		MongoCollection<Document> collection = database.getCollection("test1");//replace with not tes
+		MongoClient mongoClient = new MongoClient(host); //connects to client on localhost
+		MongoDatabase database = mongoClient.getDatabase(dbname);//gets db called mydb
+		MongoCollection<Document> collection = database.getCollection(usedcoll);//replace with not tes
 		return collection;
 	}
 	
-	public static String getdbinfo(){//get db info, probably never gonna be used, might commend out
+	public static String getDbInfo(){//get db info, probably never gonna be used, might commend out
 		MongoClient mongoClient = new MongoClient( "localhost" ); //connects to client on localhost
 		MongoDatabase database = mongoClient.getDatabase("mydb");//gets db called mydb
 		String info = database.getName();//just make a string with the name of the db
 		return info;//return
 	}
 	
-	public static void inserttodb(){//send data to database, test method using ServerStart.getTestJson()
+	public static void insertToDb(){//send data to database, test method using ServerStart.getTestJson()
 		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(ServerStart.getTestJson()); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
 	
-	public static void inserttodb(String json){//send data to database
+	public static void insertToDb(String json){//send data to database
 		MongoCollection<Document> collection = getConnection();
 		Document parsedjson = Document.parse(json); //REPLACE THIS WITH NOT TEST METHOD, create new document from JSON string
 		collection.insertOne(parsedjson); //insert document into collection
 	}
 	
-	public static long getusercount(){//gets the number of users in a db
+	public static long getUserCount(){//gets the number of users in a db
 		MongoCollection<Document> collection = getConnection();//retrieve a collection
 		long usercount = collection.count();
 		return usercount;
 	}
 	
-	
-//	public static String getfromdb(){    TODO
-//		
-//	}
+	public static String getfromdb(String field, String value){//for example, getfromdb(username, john);
+		MongoCollection<Document> collection = getConnection();//retrieve a collection
+		Document myDoc = collection.find(eq(field,value)).first();
+		return myDoc.toJson();
+	}
 
 }
