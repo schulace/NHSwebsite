89003592d316e50f16f86518d6fd65d439a27b86
George Ridgway <georger361@gmail.com>
2016-06-03 12:47:07 -0400

    error checking for ScheduleSubmit

--- webJava/ScheduleSubmit.java
+++ webJava/ScheduleSubmit.java
@@ -48,11 +48,17 @@ public class ScheduleSubmit extends HttpServlet {
 	{
 		Map<String, String[]> m = request.getParameterMap(); //for which blocks of they day we have
 		ArrayList<SchoolClass> classes = new ArrayList<SchoolClass>(); //eventually passed into a schedule constructor
+		boolean match = false; //used for error checking
 		for(int x = 1; x <= getNumberOfClasses(request); x ++)
 		{
 			String s = Integer.toString(x);
 			String[] ar = m.get(s); //gets a string array of numbers representing which days of the schedule the class meets
-			if(ar != null) //errors if not this (deals with retards not checking any days.
+			if (ar == null) //error checking for people not checking any days for their classes
+			{
+				theyDoneFuckedUp(request, response);
+			}
+			
+			else if(ar != null) //errors if not this (deals with retards not checking any days.
 			{
 				LetterDay[] days = new LetterDay[ar.length]; //new letterday array of same size as number of boxes ticked for the class.
 				for(int y = 0; y < ar.length; y ++)
@@ -63,15 +69,56 @@ public class ScheduleSubmit extends HttpServlet {
 				}
 				//i swear the below line works. it just parses the stuff that's not hard.
 				SchoolClass c = new SchoolClass(request.getParameter("className" + x), Subject.valueOf(request.getParameter("subject" + x)), days, Integer.parseInt(request.getParameter("block" + x)), new Teacher(request.getParameter("teacher" + x)));
-				classes.add(c); //adds the class generated above to an array.
+				//checks to see if the school class has already been checked more than once
+				
+				for (int i = 0; i < classes.size(); i++)
+				{
+					if (classes.get(i) == c)
+					{
+						match = true;
+					}
+					else if (classes.get(i).getName() == c.getName())
+					{
+						match = true;
+					}
+					else if ((classes.get(i).getDays() == c.getDays()) && ((classes.get(i).getBlock()) == c.getBlock()))  
+					{
+						match = true;
+					}
+					else
+					{
+						match = false;
+					}
+				}
+				
+				if (match = true) //if they enter two of the same classes
+				{
+					theyDoneFuckedUp(request, response);
+				}
+				else
+				{
+					classes.add(c); //adds the class generated above to an array.
+				}
 			}
 		}
 		StudentSchedule sched = new StudentSchedule(classes); //creates a student schedule.
 		System.out.println("\n"+sched);
 		response.getWriter().append(sched.toPrettierHTML());
-		
 	}
-	
+	//redirects them to the userInfoEntry if they fucked up
+	private void theyDoneFuckedUp (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("It seems like you have entered your shedule information incorrectly, please re-input your student shedule, you will be redirected shortly....");
+			try 
+			{
+			    Thread.sleep(3000);                 //1000 milliseconds is one second, i'd say that 3 sec is a good time for the users to read the error message
+			} catch(InterruptedException ex)
+			{
+			    Thread.currentThread().interrupt();
+			}
+			String redirectURL = "http://localhost:8080/nhsWeb/userInfoEntry.jsp"; //TODO change this once we have a domian name for the website
+		    response.sendRedirect(redirectURL);
+		    return;
+	}
 	private int getNumberOfClasses(HttpServletRequest request)
 	{
 		Enumeration<String> stuff = request.getParameterNames();
@@ -80,9 +127,6 @@ public class ScheduleSubmit extends HttpServlet {
 		{
 			stList.add(stuff.nextElement());
 		}
-		
-		return (stList.size()-2)/4;
-		
+		return (stList.size()-2)/4;	
 	}
-
 }
